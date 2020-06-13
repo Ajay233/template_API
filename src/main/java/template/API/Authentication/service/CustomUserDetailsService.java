526ffa401cde84ca6e321d3eaba7c81dab03fae3
Ajay <ajaymungur@hotmail.com>
2020-06-13 14:41:06 +0100

    Created UserPrincipal and CustomUserDetailsService to override UserDetails and UserDetailsService

---
+++ src/main/java/template/API/Authentication/service/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package template.API.Authentication.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import template.API.Authentication.model.UserPrincipal;
+import template.API.User.model.User;
+import template.API.User.repository.UserRepository;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if(user == null){
+            throw new UsernameNotFoundException("No account matching that email");
+        }
+        return new UserPrincipal(user);
+    }
+}
