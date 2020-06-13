a18ce49356c9352a6160b5b8daff869146dc141c
Ajay <ajaymungur@hotmail.com>
2020-06-13 16:31:27 +0100

    Added updated Swagger Config so JWT can be set to access and test secured endpoints

--- src/main/java/template/API/Config/SwaggerConfig.java
+++ src/main/java/template/API/Config/SwaggerConfig.java
@@ -1,13 +1,19 @@
 package template.API.Config;
 
+import com.google.common.collect.Lists;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2WebMvc
@@ -19,7 +25,46 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo())
+                .securitySchemes(Lists.newArrayList(apiKey()))
+                .securityContexts(Lists.newArrayList(securityContext()));
+    }
+
+
+    private Contact contact(){
+        return new Contact("Author", "Authour's website", "Authour's email");
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfo(
+                "Template Spring Boot API",
+                "A bare bones API using Spring Security to service an Authentication endpoint which allows account creation " +
+                        "and login.  All other endpoints are secured and only accessible where a JWT is provided as the 'Authorization' " +
+                        "within request headers",
+                "1",
+                "N/A",
+                contact(),
+                "N/A",
+                "N/A",
+                Collections.emptyList());
+    }
+
+    private ApiKey apiKey(){
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext(){
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.any())
                 .build();
     }
 
+    List<SecurityReference> defaultAuth(){
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "access secured endpoints");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Lists.newArrayList(new SecurityReference("JWT", authorizationScopes));
+    }
 }
